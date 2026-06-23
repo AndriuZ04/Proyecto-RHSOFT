acc70b3b7a17411d58a1b54722ff4acdad643c28
ALTdamage <crisesco171207@gmail.com>
2026-06-23 07:52:56 -0500

    Subida archivo

--- Proyecto-RHSOFT/database/conexion.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-conexion = mysql.connector.connect(
-    host="localhost",
-    user="root",            # Usuario por defecto en XAMPP
-    password="",            # Contraseña por defecto (vacía)
-    database="rrhh_empresa", # El nombre exacto de tu base de datos
-    port=3306               # ¡Cambiado a 3306!
-)
